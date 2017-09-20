cc7ad7a925847e7ef77fe17c83ba97a2272f827d
rogueITman <aelam71@yahoo.com>
2017-09-19 19:57:06 -0500

    intial commit, setup finish

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+var path = require('path');
+
+//tell webpack where to place the transpiled code
+var DIST_DIR = path.resolve(__dirname, 'dist');
+
+//tell webpack where to find non transpiled code
+var SRC_DIR = path.resolve(__dirname, 'src');
+
+var config = {
+    entry: SRC_DIR + '/app/index.js',
+    output: {
+        path: DIST_DIR + '/app',
+        filename: 'bundle.js',
+        publicPath: '/app/'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js?/,
+                include: SRC_DIR,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['react', 'es2015', 'stage-2']
+                }
+            }
+        ]
+    }
+};
+
+module.exports = config;
